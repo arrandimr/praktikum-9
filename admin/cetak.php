44e839077036a41778bc9b828078939477f73d93
arrandimr <61189026+arrandimr@users.noreply.github.com>
2020-04-02 19:27:30 +0700

    Update cetak.php

--- admin/cetak.php
+++ admin/cetak.php
@@ -6,21 +6,21 @@ echo "<table width='75%' cellpadding='2' cellspacing='0' border='1'>
 <tr>
 	<th>No</th>
 	<th>Nama</th>
+	<th>jenis kelamin</th>
 	<th>Email</th>
 	<th>Alamat</th>
 	<th>Kota</th>
-	<th>Pesan</th>
-	<th></th>"; 
+	<th>Pesan</th>";  
 $no=1; 
 while($hasil=mysqli_fetch_row($qry)){
 	echo "<tr>
 			<td>$no</td> 
-			<td>$hasil[0]</td> 
 			<td>$hasil[1]</td> 
 			<td>$hasil[2]</td> 
 			<td>$hasil[3]</td> 
 			<td>$hasil[4]</td> 
-			<td>$hasil[5]</td>  
+			<td>$hasil[5]</td> 
+			<td>$hasil[6]</td>  
 		</tr>"; $no++; } 
 echo "</table>"; 
 ?>  
